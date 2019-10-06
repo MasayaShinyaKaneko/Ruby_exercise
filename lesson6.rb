b28b579af1a2602f0fc0866d012c2647b0d2373c
MasayaShinyaKaneko <mkanekodays@gmail.com>
2019-10-06 11:47:40 +0900

    [finish]2-7

---
+++ lesson6.rb
@@ -0,0 +1,20 @@
+total_price = 90
+
+if total_price > 100
+	puts "みかんを購入。所持金に余りあり"
+end
+if total_price == 100
+	puts "みかんを購入。所持金は0円"
+end
+if total_price < 100
+	puts "みかんを購入することができません"
+end
+
+
+if total_price > 100
+	puts "みかんを購入。所持金に余りあり"
+elsif total_price == 100
+	puts "みかんを購入。所持金は0円"
+else
+	puts "みかんを購入することができません"
+end
